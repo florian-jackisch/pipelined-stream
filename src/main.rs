8e9123b9dc5d440d7eda6f19457122a5a2327b05
Florian Jackisch <f.jackisch@ifn.ing.tu-bs.de>
2017-02-14 14:41:31 +0100

    Add stream of ints

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate futures;
+
+use futures::{Future, Sink, Stream};
+
 fn main() {
-    println!("Hello, world!");
+    // produce input
+    let (mut tx, rx) = futures::sync::mpsc::channel(10);
+    let handle = std::thread::spawn(|| {
+        for i in 1..10 {
+            tx = tx.send(i).wait().unwrap();
+        }
+    });
+
+    let rx = rx.and_then(|i| {
+        println!("{}", i);
+        Ok(i)
+    });
+
+    rx.collect().wait().unwrap();
+
+    handle.join().unwrap();
 }
