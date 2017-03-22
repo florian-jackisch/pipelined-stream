407d4c27662192dacc5526123c9e63c72e8d81d7
Florian Jackisch <f.jackisch@ifn.ing.tu-bs.de>
2017-03-22 06:42:52 +0100

    Use buffered

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,23 @@ fn sleep_random() {
     std::thread::sleep(std::time::Duration::from_millis(sleep_time_ms as u64));
 }
 
+/// Sleep for a random time, then print an integer.
+fn sleep_and_print(stage: i32, i: i32) -> Result<i32, ()> {
+    sleep_random();
+    println!("Stage {} received {} (in thread \"{}\")",
+             stage,
+             i,
+             std::thread::current().name().unwrap());
+    Ok(i)
+}
+
 fn main() {
     // produce input
     let (mut tx, rx) = futures::sync::mpsc::channel(3);
     let handle = std::thread::Builder::new()
         .name("source".to_string())
         .spawn(|| {
-            for i in 1..10 {
+            for i in 0..10 {
                 sleep_random();
                 println!("Source  produced {} (in thread \"{}\")",
                          i,
@@ -32,33 +42,12 @@ fn main() {
         .create();
 
     // process input
-    let rx = rx.and_then(|i| {
-            pool.spawn_fn(move || {
-                sleep_random();
-                println!("Stage 0 received {} (in thread \"{}\")",
-                         i,
-                         std::thread::current().name().unwrap());
-                Ok(i)
-            })
-        })
-        .and_then(|i| {
-            pool.spawn_fn(move || {
-                sleep_random();
-                println!("Stage 1 received {} (in thread \"{}\")",
-                         i,
-                         std::thread::current().name().unwrap());
-                Ok(i)
-            })
-        })
-        .and_then(|i| {
-            pool.spawn_fn(move || {
-                sleep_random();
-                println!("Stage 2 received {} (in thread \"{}\")",
-                         i,
-                         std::thread::current().name().unwrap());
-                Ok(i)
-            })
-        });
+    let rx = rx.map(|i| pool.spawn_fn(move || sleep_and_print(0, i)))
+        .buffered(10)
+        .map(|i| pool.spawn_fn(move || sleep_and_print(1, i)))
+        .buffered(10)
+        .map(|i| pool.spawn_fn(move || sleep_and_print(2, i)))
+        .buffered(10);
 
     // consume stream
     for r in rx.wait() {
